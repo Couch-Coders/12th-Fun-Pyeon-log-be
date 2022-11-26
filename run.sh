4cfbd0066da3770346c445929dfa02e439f24203
DoKyeong Oh <dkproh@gmail.com>
2022-11-26 16:50:46 +0900

    오타 수정 	run.sh에서 demo-0.0.1-SNAPSHOT-plain.jar -> demo-0.0.1-SNAPSHOT.jar 수정

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 echo ${SECRET_FILE} >> env.json
 sed 's/*/"/g' env.json >> secureFile.json
-java -Dserver.port=$PORT $JAVA_OPTS -Dspring.profiles.actice=prod -jar ./build/libs/demo-0.0.1-SNAPSHOT-plain.jar
+java -Dserver.port=$PORT $JAVA_OPTS -Dspring.profiles.actice=prod -jar ./build/libs/demo-0.0.1-SNAPSHOT.jar
