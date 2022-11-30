472e7e786b372f004d0c90a3e61eecd5b0ff1b7d
DoKyeong Oh <Dkproh@gmail.com>
2022-11-30 17:18:08 +0900

    feat : StoreDTO 추가
    각 편의점에 대해 DB에 저장되는 값들을 가공해서 Front에 보내주기 위한 DTO 추가

---
+++ src/main/java/com/example/demo/dto/StoreDTO.java
@@ -0,0 +1,27 @@
+package com.example.demo.dto;
+
+import com.example.demo.entity.Keyword;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class StoreDTO {
+    String id;
+    Double StarCount;
+    Integer reviewCount;
+
+    List<String> keywordList;
+
+    @Builder
+    public StoreDTO(String id, Double starCount, Integer reviewCount, List<String> keywordList) {
+        this.id = id;
+        this.StarCount = starCount;
+        this.reviewCount = reviewCount;
+        this.keywordList = keywordList;
+    }
+
+    public StoreDTO() {
+    }
+}
