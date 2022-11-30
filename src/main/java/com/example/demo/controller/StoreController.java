dbbd86b9128c2ac7c9fc75098d4374ca2428de0a
DoKyeong Oh <Dkproh@gmail.com>
2022-11-30 16:09:29 +0900

    feat : StoreController 추가
    StoreController는 편의점 요약 정보 요청을 처리할 컨트롤러이다.

--- src/main/java/com/example/demo/controller/StoreController.java
+++ src/main/java/com/example/demo/controller/StoreController.java
@@ -1,16 +1,25 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.StoreDTO;
+import com.example.demo.service.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/stores")
 public class StoreController {
 
+    @Autowired
+    StoreService storeService;
+
     @GetMapping("")
-    public Object getStoreSummary(){
-        return null;
+    public List<StoreDTO> storeSummaries(@RequestParam(value = "id", required = false) String[] storeIds){
+        return storeService.getStoreSummaries(storeIds);
     }
 
 }
