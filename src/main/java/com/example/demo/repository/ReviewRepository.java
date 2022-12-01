34c0b78534b33ce2efc097d5103e3403c05d0368
DoKyeong Oh <Dkproh@gmail.com>
2022-12-01 11:52:12 +0900

    feat : findByStoreId() 추가
    리뷰 테이블에서 리뷰를 store_id로 검색해오는 메소드

--- src/main/java/com/example/demo/repository/ReviewRepository.java
+++ src/main/java/com/example/demo/repository/ReviewRepository.java
@@ -4,6 +4,9 @@ import com.example.demo.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByStoreId(String storeId);
 }
