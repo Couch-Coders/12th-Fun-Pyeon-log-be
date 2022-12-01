e9b37abe36104a66457b24ac1ca647a8f609278f
DoKyeong Oh <Dkproh@gmail.com>
2022-12-01 13:37:26 +0900

    chore : minor (무시하셔도 됩니다)

---
+++ src/test/java/com/example/demo/service/StoreServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.ReviewRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class StoreServiceTest {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @Test
+    void getStoreSummaries() {
+    }
+}
