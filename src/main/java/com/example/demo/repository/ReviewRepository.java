72e06890b901f128e2e974a31ca9da93bbfcda2d
DoKyeong Oh <Dkproh@gmail.com>
2022-12-01 13:37:01 +0900

    refactor : findAllByStoreId() -> findAllByStoreIdIn() 메소드 변경
    기존에 StoreService.getStoreSummaries()메소드 한 번에 여러 번의 findAllByStoreId()를 실행하게 되어있었는데,
    이러면 여러 번의 트랜잭션이 발생하고 트래픽이 증가할 것으로 예상되어 한 번의 조회로 끝낼 수 있도록 findAllByStoreIdIn() 메서드로 변경

--- src/main/java/com/example/demo/repository/ReviewRepository.java
+++ src/main/java/com/example/demo/repository/ReviewRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findAllByStoreId(String storeId);
 
+    List<Review> findAllByStoreIdIn(String[] storeIds);
+
 }
