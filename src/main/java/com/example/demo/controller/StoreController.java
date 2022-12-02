d7a06290d4717b4abb07e3811d55b02286a913d2
DoKyeong Oh <Dkproh@gmail.com>
2022-12-02 16:40:14 +0900

    feat : 편의점 요약 정보 리턴 객체 타입 변경 StoreSummary -> StoreDTO
    StoreSummary를 Json으로 변경하다가 스택 오버플로우 에러가 발생.
    현재는 StoreSummary의 필드에 StoreKeyword타입이 있는데, StoreKeyword의 필드에 StoreSummary타입의 필드가 있어서 스프링에서 Json으로 변경해주는 메세지 컨버터에서 스택 오버플로우가 발생한 것으로 추정됨

--- src/main/java/com/example/demo/controller/StoreController.java
+++ src/main/java/com/example/demo/controller/StoreController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.StoreDTO;
+import com.example.demo.dto.StoreSummaryDTO;
+import com.example.demo.entity.StoreSummary;
 import com.example.demo.service.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +16,12 @@ public class StoreController {
     StoreService storeService;
 
     @GetMapping("")
-    public List<StoreDTO> storeSummaries(@RequestParam(value = "id", required = false) String[] storeIds){
+    public List<StoreSummaryDTO> storeSummaries(@RequestParam(value = "id", required = false) String[] storeIds){
         return storeService.getStoreSummaries(storeIds);
     }
 
     @GetMapping("/{storeId}")
-    public StoreDTO storeSummary(@PathVariable String storeId){
+    public StoreSummaryDTO storeSummary(@PathVariable String storeId){
         return storeService.getStoreSummary(storeId);
     }
 
