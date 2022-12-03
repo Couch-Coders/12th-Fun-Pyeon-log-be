ab3084488672776f4b4bcd476bb4c2e5f6d03205
DoKyeong Oh <Dkproh@gmail.com>
2022-12-03 15:23:53 +0900

    feat : StoreSummary의 엔티티관계 추가 및 연관된 기능추가
    - StoreSummary와 StoreKeyword를 1:N관계가 될 수 있도록 클래스 수정
    - List형태의 StoreKeyword에서 실제 키워드 값을 꺼내는 getKeywordContents()메소드 추가
    - List<StoreKeyword>타입의 멤버필드를 키워드 갯수가 많은 것을 기준으로 정렬하는 메소드인 sortByKeywordCount() 추가

--- src/main/java/com/example/demo/entity/StoreSummary.java
+++ src/main/java/com/example/demo/entity/StoreSummary.java
@@ -1,16 +1,19 @@
 package com.example.demo.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
 @Getter
 public class StoreSummary {
 
-    @Id @GeneratedValue
-    Integer storeId;
+    @Id
+    String storeId;
 
     @Column
     Double starRate;
@@ -18,7 +21,30 @@ public class StoreSummary {
     @Column
     Long reviewCount;
 
-    @OneToMany(mappedBy = "store")
-    List<Keyword> keywords;
+    @OneToMany(mappedBy = "storeSummary")
+    List<StoreKeyword> storeKeywords;
 
+    @Builder
+    public StoreSummary(String storeId, Double starRate, Long reviewCount, List<StoreKeyword> storeKeywords) {
+        this.storeId = storeId;
+        this.starRate = starRate;
+        this.reviewCount = reviewCount;
+        this.storeKeywords = storeKeywords;
+    }
+
+    public List<String> getKeywordContents(int keywordsSizeLimit){
+        List<String> keywords = new ArrayList<>();
+        if (storeKeywords.size() >= keywordsSizeLimit)
+            storeKeywords = storeKeywords.subList(0, keywordsSizeLimit);
+        for (int i=0 ; i<storeKeywords.size() ; i++)
+            keywords.add(storeKeywords.get(i).getKeywordContent().getKeywordContent());
+        return keywords;
+    }
+
+    public void sortByKeywordCount(){
+        Collections.sort(storeKeywords, (o1, o2) -> (int) -(o1.getKeywordCount() - o2.getKeywordCount()));
+    }
+
+    public StoreSummary() {
+    }
 }
