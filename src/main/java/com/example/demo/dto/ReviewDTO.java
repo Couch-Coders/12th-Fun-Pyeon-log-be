43385040b404b3c778986de83698273c79226c34
DoKyeong Oh <Dkproh@gmail.com>
2022-12-05 20:27:14 +0900

    feat : ReviewDTO를 생성할 때 Review객체를 받아서 생성하도록 작성
    review를 받아서 ReviewDTO를 만들 때 List<Keyword>타입의 keywords를 List<String> 타입으로 타입변환하는 메소드인 castKeywordsToStrings()메소드 작성

--- src/main/java/com/example/demo/dto/ReviewDTO.java
+++ src/main/java/com/example/demo/dto/ReviewDTO.java
@@ -1,10 +1,11 @@
 package com.example.demo.dto;
 
 import com.example.demo.entity.Keyword;
+import com.example.demo.entity.Review;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -20,5 +21,25 @@ public class ReviewDTO {
     List<String> keywords;
 
     public ReviewDTO() {
+    }
+
+    public ReviewDTO(Review review) {
+        this.reviewEntryNo = review.getReviewEntryNo();
+        this.reviewContent = review.getReviewContent();
+        this.starCount = review.getStarCount();
+        this.createdDate = review.getCreatedDate();
+        this.modifiedDate = review.getModifiedDate();
+        this.userEntryNo = review.getUser().getUserEntryNo();
+        this.storeId = review.getStoreId();
+        castKeywordsToStrings(review.getKeywords());
+    }
+
+    public void castKeywordsToStrings(List<Keyword> keywords){
+        if (keywords == null)
+            return;
+        if (this.keywords == null)
+            this.keywords = new ArrayList<>();
+        for (Keyword k : keywords)
+            this.keywords.add(k.getKeywordContent().getKeywordContent());
     }
 }
