b459ce9501539635878f790ec6175fb29d82a855
DoKyeong Oh <Dkproh@gmail.com>
2022-12-05 20:43:18 +0900

    feat : FirebaseInitializer 빈 설정 추가
    FilebaseApp, FilebaseAuth을 빈으로 등록하도록 작성

--- src/main/java/com/example/demo/config/FirebaseInitializer.java
+++ src/main/java/com/example/demo/config/FirebaseInitializer.java
@@ -1,8 +1,34 @@
 package com.example.demo.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @Configuration
 public class FirebaseInitializer {
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("./filebase.json");
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("")
+                .build();
+
+        FirebaseApp app = FirebaseApp.initializeApp(options);
+        return app;
+    }
+
+    @Bean
+    public FirebaseAuth getFirebaseAuth() throws IOException {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance(firebaseApp());
+        return firebaseAuth;
+    }
 
 }
