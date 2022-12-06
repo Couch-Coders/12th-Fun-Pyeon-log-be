803aa8e3936ed402768afdb3443eb5955eddfa9f
DoKyeong Oh <Dkproh@gmail.com>
2022-12-06 17:11:13 +0900

    feat : FirebaseTokenFilter의 필터 메소드 작성
    doFilterInternal() 메소드 작성 : request.header("Authorization")에서 엑세스 토큰을 받아서 유효성을 체크하고 유저를 획득하는 메소드

--- src/main/java/com/example/demo/filter/FirebaseTokenFilter.java
+++ src/main/java/com/example/demo/filter/FirebaseTokenFilter.java
@@ -1,7 +1,12 @@
 package com.example.demo.filter;
 
+import com.example.demo.service.UserService;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,7 +23,30 @@ public class FirebaseTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        FirebaseToken decodedToken;
+        String header = request.getHeader("Authorization");
+        if (header == null || !header.startsWith("Bearer "))
+            setUnauthorizedResponse(response, "INVALID_TOKEN");
 
+        String token = header.substring(7);
+        try {
+            decodedToken = firebaseAuth.verifyIdToken(token);
+        } catch (FirebaseAuthException e) {
+            setUnauthorizedResponse(response, "INVALID_TOKEN");
+            return;
+        }
+
+        try {
+            UserDetails user = userDetailsService.loadUserByUsername(decodedToken.getEmail());
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        } catch (Exception e) {
+            setUnauthorizedResponse(response, "INVALID_TOKEN");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
     }
 
     private void setUnauthorizedResponse(HttpServletResponse response, String code) throws IOException {
