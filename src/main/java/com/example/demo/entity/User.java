9c01c6cc0ce2c3423689523d2436484c2cc96f6b
DoKyeong Oh <Dkproh@gmail.com>
2022-12-06 22:20:04 +0900

    feat : User에서 UserDetails를 상속받고 오버라이드 메소드 추가

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -3,14 +3,17 @@ package com.example.demo.entity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Date;
 
 @Entity(name = "users")
 @Getter
 @ToString
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     Long userEntryNo;
@@ -32,4 +35,38 @@ public class User {
 
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
