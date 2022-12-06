64643fe85c0e9f078fe9a4013f588f114d910cae
DoKyeong Oh <Dkproh@gmail.com>
2022-12-06 22:19:32 +0900

    feat : UserService에 UserDetailsService를 상속 추가

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -5,6 +5,9 @@ import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -12,7 +15,7 @@ import java.util.Date;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
 
@@ -40,5 +43,10 @@ public class UserService {
     public Long deleteUser(Long no) {
         userRepository.deleteById(no);
         return no;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return null;
     }
 }
