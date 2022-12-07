9405ea9036aaba59ae06b00c05b949acc9f90fcf
DoKyeong Oh <Dkproh@gmail.com>
2022-12-08 00:15:30 +0900

    feat : FirebaseTokenFilter 추상화
    - 실제 엑세스 토큰이 존재하지 않는 경우 내부 코드 테스트를 위해 추상화하여 InterFirebaseFilter로 명명.
    - 실제 사용할 필터는 ProdFirebaseFilter이다.

--- src/main/java/com/example/demo/filter/FirebaseTokenFilter.java
+++ src/main/java/com/example/demo/filter/FirebaseTokenFilter.java
@@ -1,57 +1,32 @@
 package com.example.demo.filter;
 
-import com.example.demo.service.UserService;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseToken;
 import lombok.AllArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @AllArgsConstructor
-public class FirebaseTokenFilter extends OncePerRequestFilter {
+public abstract class FirebaseTokenFilter extends OncePerRequestFilter {
+    protected UserDetailsService userDetailsService;
+    protected FirebaseAuth firebaseAuth;
 
-    private UserDetailsService userDetailsService;
-    private FirebaseAuth firebaseAuth;
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        FirebaseToken decodedToken;
-        String header = request.getHeader("Authorization");
-        if (header == null || !header.startsWith("Bearer "))
-            setUnauthorizedResponse(response, "INVALID_TOKEN");
-
-        String token = header.substring(7);
-        try {
-            decodedToken = firebaseAuth.verifyIdToken(token);
-        } catch (FirebaseAuthException e) {
-            setUnauthorizedResponse(response, "INVALID_TOKEN");
-            return;
+    protected String findCookie(Cookie[] cookies, String cookieName) {
+        for (Cookie c : cookies) {
+            if (c.getName().equals(cookieName)) {
+                log.info(cookieName + " : " + c.getValue());
+                return c.getValue();
+            }
         }
-
-        try {
-            UserDetails user = userDetailsService.loadUserByUsername(decodedToken.getEmail());
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                    user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-        } catch (Exception e) {
-            setUnauthorizedResponse(response, "INVALID_TOKEN");
-            return;
-        }
-
-        filterChain.doFilter(request, response);
+        return null;
     }
 
-    private void setUnauthorizedResponse(HttpServletResponse response, String code) throws IOException {
+    protected void setUnauthorizedResponse(HttpServletResponse response, String code) throws IOException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
         response.getWriter().write("{\"code\" : \""+code+"\"}");
