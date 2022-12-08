892252272dc0cdf6e5bf52a48392e7e34245db3d
DoKyeong Oh <Dkproh@gmail.com>
2022-12-08 17:50:26 +0900

    feat : 쿠키 생성, 삭제 기능 추가
    - 컨트롤러에서 ResponseCookie 객체를 사용해서 쿠키 발행 및 삭제하는 메소드 추가

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.UserDTO;
+import com.example.demo.dto.FirebaseTokenDTO;
 import com.example.demo.entity.User;
+import com.example.demo.service.AbstractAuthService;
 import com.example.demo.service.UserService;
+import com.google.firebase.auth.FirebaseAuthException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -31,18 +36,32 @@ public class UserController {
         return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, removeCookie("token").toString()).build();
     }
 
-    @PostMapping("")
-    public User insertUser(@RequestBody UserDTO userDTO){
-        return userService.addUser(userDTO);
     @DeleteMapping("")
     public void deleteUser(@RequestHeader("Authorization") String token) throws FirebaseAuthException {
         FirebaseTokenDTO tokenDTO = authService.verifyIdToken(token);
         userService.deleteUser(tokenDTO.getEmail());
     }
 
-    @DeleteMapping("/{no}")
-    public Long deleteUser(@PathVariable Long no){
-        return userService.deleteUser(no);
+    public ResponseCookie createCookie(String key, String value){
+        return ResponseCookie.from(key, value)
+                .maxAge(1 * 60 * 60 * 24 * 365)
+                .sameSite("Lax")
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .build();
     }
+
+    public ResponseCookie removeCookie(String key){
+        return ResponseCookie.from(key, "")
+                .maxAge(0)
+                .sameSite("Lax")
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .build();
+    }
+
+
 
 }
