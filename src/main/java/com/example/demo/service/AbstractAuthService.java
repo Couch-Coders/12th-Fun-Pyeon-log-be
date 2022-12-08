ec67dbe48aa1b3fbcbd1e64679ea69adcb556e31
DoKyeong Oh <Dkproh@gmail.com>
2022-12-08 17:56:25 +0900

    feat : AbstractAuthService 추가
    아래 기능을 수행할 AuthService를 추가
    - 파이어베이스 토큰 유효성 체크
    - 리프레쉬 토큰 초기화
    - 로그인/회원가입

---
+++ src/main/java/com/example/demo/service/AbstractAuthService.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.FirebaseTokenDTO;
+import com.example.demo.entity.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public abstract class AbstractAuthService {
+
+    @Autowired
+    FirebaseAuth firebaseAuth;
+
+    @Autowired
+    UserService userService;
+
+    public abstract FirebaseTokenDTO verifyIdToken(String bearerToken) throws FirebaseAuthException;
+
+    public abstract void revokeRefreshTokens(String uid) throws FirebaseAuthException;
+
+    public User loginOrEntry(FirebaseTokenDTO tokenDTO) {
+        User user = null;
+        try {
+            user = userService.getUser(tokenDTO.getEmail());
+        } catch (Exception e) {
+            log.error("User with email {} was not found in the database, creating user", tokenDTO.getEmail());
+            user = userService.addUser(tokenDTO.getEmail());
+        }
+
+        return user;
+    }
+}
