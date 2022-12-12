36b036269ea16018783f816cb2c1cdb72531cb3b
DoKyeong Oh <Dkproh@gmail.com>
2022-12-12 14:09:15 +0900

    feat : KeywordRepository.findBtStoreId() 추가

--- src/main/java/com/example/demo/repository/KeywordRepository.java
+++ src/main/java/com/example/demo/repository/KeywordRepository.java
@@ -4,7 +4,11 @@ import com.example.demo.entity.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
     void deleteByStoreId(String storeId);
+
+    List<Keyword> findByStoreId(String storeId);
 }
