afa21eb82849fe22e6722eb0f1bcb0c8e3cbea70
DoKyeong Oh <Dkproh@gmail.com>
2022-12-16 23:01:50 +0900

    feat : ProdAuthService 생성자 내용 수정
    기존에는 상위 클래스의 생성자를 그대로 실행하도록 했었지만, FirebaseAuth객체를 따로 가지고 있기 때문에 빈을 주입받아서 멤버에 주입하도록 수정

---
+++ src/main/java/com/example/demo/service/auth/ProdAuthService.java
@@ -0,0 +1,45 @@
+package com.example.demo.service.auth;
+
+import com.example.demo.dto.FirebaseTokenDTO;
+import com.example.demo.service.UserService;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Slf4j
+@Service
+@Profile("prod")
+public class ProdAuthService extends AuthService {
+    FirebaseAuth firebaseAuth;
+    public ProdAuthService(FirebaseAuth firebaseAuth, UserService userService) {
+        super(userService);
+        this.firebaseAuth = firebaseAuth;
+    }
+
+    @Override
+    public FirebaseTokenDTO verifyIdToken(String bearerToken) {
+        try {
+            FirebaseToken token = firebaseAuth.verifyIdToken(bearerToken);
+            FirebaseTokenDTO tokenDTO = new FirebaseTokenDTO(token);
+            return tokenDTO;
+        } catch (Exception e) {
+            log.error("access token is not usable : {}", e.getMessage());
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "토큰 값이 유효하지 않습니다!");
+        }
+    }
+
+    @Override
+    public void revokeRefreshTokens(String uid) throws FirebaseAuthException {
+        try {
+            firebaseAuth.revokeRefreshTokens(uid);
+        } catch (FirebaseAuthException e) {
+            log.error("revoke token error : {}", e.getMessage());
+            throw new FirebaseAuthException(e);
+        }
+    }
+}
