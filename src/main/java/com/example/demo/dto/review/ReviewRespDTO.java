cc5fdcc60bdf4ee6a7a2e6c3785d55e7cadf4baa
DoKyeong Oh <Dkproh@gmail.com>
2022-12-18 14:50:35 +0900

    feat : ReviewRespDTO 추가
    리뷰 조회 요청을 응답하기 위한 정보를 저장할 DTO

---
+++ src/main/java/com/example/demo/dto/review/ReviewRespDTO.java
@@ -0,0 +1,46 @@
+package com.example.demo.dto.review;
+
+import com.example.demo.entity.Keyword;
+import com.example.demo.entity.Review;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+public class ReviewRespDTO {
+    Long reviewEntryNo;
+    String reviewContent;
+    Double starCount;
+    LocalDateTime createdDate;
+    LocalDateTime modifiedDate;
+    String userEmail;
+    String storeId;
+    List<String> keywords;
+
+    public ReviewRespDTO() {
+    }
+
+    public ReviewRespDTO(Review review) {
+        this.reviewEntryNo = review.getReviewEntryNo();
+        this.reviewContent = review.getReviewContent();
+        this.starCount = review.getStarCount();
+        this.createdDate = review.getCreatedDate();
+        this.modifiedDate = review.getModifiedDate();
+        this.userEmail = review.getUser().getEmail();
+        this.storeId = review.getStoreId();
+        castKeywordsToStrings(review.getKeywords());
+    }
+
+    public void castKeywordsToStrings(List<Keyword> keywords){
+        if (keywords == null)
+            return;
+        if (this.keywords == null)
+            this.keywords = new ArrayList<>();
+        for (Keyword k : keywords)
+            this.keywords.add(k.getKeywordContent().getKeywordContent());
+    }
+
+}
