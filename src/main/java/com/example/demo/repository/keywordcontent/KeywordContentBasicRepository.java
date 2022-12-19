fbadcc3dc61bea90ac15a5cdf2cc71123dcb3e89
DoKyeong Oh <Dkproh@gmail.com>
2022-12-19 21:25:35 +0900

    feat : KeywordContentBasicRepository 추가
    KeywordContentRepository - KeywordContentCustomRepository 간 순환 참조에 걸리지 않기위한 중간 인터페이스 추가

---
+++ src/main/java/com/example/demo/repository/keywordcontent/KeywordContentBasicRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository.keywordcontent;
+
+
+import com.example.demo.entity.KeywordContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KeywordContentBasicRepository extends JpaRepository<KeywordContent, Integer> {
+
+}
