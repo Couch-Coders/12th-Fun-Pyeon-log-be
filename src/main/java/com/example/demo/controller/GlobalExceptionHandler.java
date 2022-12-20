1e8becad1a793690845779cd307a1f65834a019e
DoKyeong Oh <Dkproh@gmail.com>
2022-12-21 01:04:56 +0900

    fix : GlobalExceptionHandler 내용 일부 수정
    - @RestControllerAdvice에 basePackages 옵션 추가 : 슬래쉬(/)가 아닌 점(.)을 기준으로 탐색되는 것을 확인
    - @ExceptionHandler에 exception class옵션 추가

--- src/main/java/com/example/demo/controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/controller/GlobalExceptionHandler.java
@@ -2,16 +2,17 @@ package com.example.demo.controller;
 
 import com.example.demo.exception.CustomException;
 import com.example.demo.exception.ErrorResponse;
+import com.example.demo.filter.FirebaseTokenFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.example.demo")
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler
+    @ExceptionHandler(CustomException.class)
     public ResponseEntity<Object> handleCustomException(CustomException e){
         return ErrorResponse.toResponseEntity(e);
     }
