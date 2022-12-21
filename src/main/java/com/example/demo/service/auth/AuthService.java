a609e32575db8dd4b979a6ca7ec0a3e2d7270ddd
DoKyeong Oh <Dkproh@gmail.com>
2022-12-21 15:51:06 +0900

    feat : AuthService의 catch에 Exception을 구체화
    - (Exception e) -> (CustomException e)로 수정
    - CustomException으로 일괄 변경하는 작업 진행 중

--- src/main/java/com/example/demo/service/auth/AuthService.java
+++ src/main/java/com/example/demo/service/auth/AuthService.java
@@ -2,6 +2,7 @@ package com.example.demo.service.auth;
 
 import com.example.demo.dto.FirebaseTokenDTO;
 import com.example.demo.entity.User;
+import com.example.demo.exception.CustomException;
 import com.example.demo.service.UserService;
 import com.google.firebase.auth.FirebaseAuthException;
 import lombok.AllArgsConstructor;
@@ -22,7 +23,7 @@ public abstract class AuthService {
         User user = null;
         try {
             user = userService.getUser(tokenDTO.getEmail());
-        } catch (Exception e) {
+        } catch (CustomException e) {
             log.error("User with email {} was not found in the database, creating user", tokenDTO.getEmail());
             user = userService.addUser(tokenDTO.getEmail());
         }
