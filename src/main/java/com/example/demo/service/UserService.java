59e2aa1fc6e421b135cbe44552d4f5a3589e70a1
DoKyeong Oh <Dkproh@gmail.com>
2022-12-21 17:20:10 +0900

    feat : UserService.getActiveUser() 추가
    email로 db에서 유저를 가져왔을 때 비활성화된 유저라면 exception을 throw시키고, 활성화 상태라면 해당 유저를 리턴해주는 메소드

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,23 +1,16 @@
 package com.example.demo.service;
 
-import com.example.demo.dto.UserDTO;
+import com.example.demo.consts.UserActiveStatus;
 import com.example.demo.entity.User;
 import com.example.demo.exception.CustomException;
 import com.example.demo.exception.ErrorCode;
 import com.example.demo.repository.UserRepository;
-import com.google.firebase.auth.FirebaseAuth;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Date;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -28,6 +21,13 @@ public class UserService implements UserDetailsService {
     public User getUser(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER, "존재하지 않는 유저입니다."));
+    }
+
+    public User getActiveUser(String email) {
+        User user = getUser(email);
+        if (!user.isActiveUser())
+            throw new CustomException(ErrorCode.NOT_CORRECT_USER, "비활성화 유저입니다. 다시 로그인 해주세요");
+        return user;
     }
 
     public User addUser(String email) {
